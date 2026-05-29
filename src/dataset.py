64f5ec56eed319ff59e099f1fc853e91d431520e
이재혁 <cncnvmvm@gmail.com>
2026-05-29 20:54:43 +0900

    dataset.py 구현

--- src/dataset.py
+++ src/dataset.py
@@ -25,12 +25,14 @@ class GPTDataset(Dataset):
         self.stride = stride if stride is not None else context_length
         # TODO: 만들 수 있는 학습 샘플 개수를 self._length에 저장하세요.
         #raise NotImplementedError("GPTDataset.__init__에서 self._length를 구현하세요.")
+        #window를 몇 개 만들어야할지 계산
         self._length = 1 + (len(token_ids) - self.context_length) // self.stride  
-        
+
 
     def __len__(self) -> int:
         """TODO: 전체 샘플 개수를 반환합니다."""
-        raise NotImplementedError("GPTDataset.__len__을 구현하세요.")
+        #raise NotImplementedError("GPTDataset.__len__을 구현하세요.")
+        return self._length
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         """
@@ -40,7 +42,17 @@ class GPTDataset(Dataset):
             input_ids: (context_length,)
             target_ids: (context_length,)
         """
-        raise NotImplementedError("GPTDataset.__getitem__을 구현하세요.")
+        #raise NotImplementedError("GPTDataset.__getitem__을 구현하세요.")
+        start = idx * self.stride
+        input_ids = []
+        target_ids = []            
+        for i in range(self.context_length):
+            input_ids.append(self.token_ids[i+start])
+        for i in range(1,self.context_length+1):
+            target_ids.append(self.token_ids[i+start])
+
+
+        return torch.tensor(input_ids, dtype=torch.long), torch.tensor(target_ids, dtype=torch.long)
 
 
 def create_dataloader(
@@ -53,4 +65,16 @@ def create_dataloader(
     num_workers: int = 0,
 ) -> DataLoader:
     """TODO: GPTDataset을 만들고 torch.utils.data.DataLoader로 감싸 반환합니다."""
-    raise NotImplementedError("create_dataloader를 구현하세요.")
+    #raise NotImplementedError("create_dataloader를 구현하세요.")
+    #GPTDataset객체를 하나 만들고, 그 객체를 dataset변수에 담는다.
+    dataset = GPTDataset(token_ids = token_ids, 
+                         context_length = context_length, 
+                         stride = stride)
+    #GPTDataset에서 샘플을 꺼내 batch 단위로 묶어주는 DataLoader를 생성
+    loader = DataLoader(dataset,
+                        batch_size=batch_size,
+                        shuffle = shuffle,
+                        drop_last = drop_last,
+                        num_workers = num_workers)
+    
+    return loader
