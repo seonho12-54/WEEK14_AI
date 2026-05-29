acce4386ffe3b35148016d57448abf5f1480224a
seonho12-54 <seonho12-54@users.noreply.github.com>
2026-05-29 05:10:09 +0000

    Continue BPE train implementation

--- src/bpe.py
+++ src/bpe.py
@@ -101,14 +101,14 @@ class BPETokenizer:
 
         #4단계 lst2 안에 있는 페어중에 베스트 페어를 ID 값으로 치환하기
         lst3 =[]
-        for i in range(len(lst2) - 1):
+        i= 0
+        while(i< len(lst2)):
           if (lst2[i],lst2[i+1]) == best_pair:
             lst3.append(new_id)
             i += 2
           else:
             lst3.append(lst2[i])
-            i += 1 
-
+            i += 1
 
     def save(self, path: str | Path):
         """
