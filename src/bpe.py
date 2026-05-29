d7487a3e7aaae8414b628b249ba062cd8ad2ed94
seonho12-54 <seonho12-54@users.noreply.github.com>
2026-05-29 10:59:18 +0000

    Continue BPE train merge replacement

--- src/bpe.py
+++ src/bpe.py
@@ -43,6 +43,8 @@ class BPETokenizer:
         1. 특수 토큰 4개를 고정 ID 0~3에 등록합니다.
         2. byte 0~255를 ID 4~259에 bytes([byte_value]) 형태로 등록합니다.
         """
+      
+
         raise NotImplementedError("_init_special_tokens를 구현하세요.")
 
     def get_pad_id(self):
